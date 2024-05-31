f89a7f1c09b8bb2da58c3797d35c500c5a9e5fbd
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-05-31 05:54:41 +0200

    implement reading

--- simple-crm/src/app/user/user.component.ts
+++ simple-crm/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,27 +7,41 @@ import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { FirebaseService } from '../services/firebase.service';  // Importieren Sie den FirebaseService
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterLink, MatButtonModule, MatIconModule, MatTooltipModule, MatCardModule],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatTooltipModule, MatCardModule,CommonModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss',
+  styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
-user = new User();
-birthDate: Date = new Date();
-
-
-  constructor(private router: Router, public dialog: MatDialog) {}
-  ngOnInit(): void {
-  }
+export class UserComponent implements OnInit, OnDestroy {
+  users: User[] = [];  // Halten Sie die Benutzerdaten in einem Array
+  private usersSubscription: Subscription | undefined;
+  user = new User();
+  birthDate: Date = new Date();
 
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = new FormControl(this.positionOptions[1]);
+
+  constructor(private router: Router, public dialog: MatDialog, private firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.usersSubscription = this.firebaseService.users$.subscribe((users) => {
+      this.users = users;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.usersSubscription) {
+      this.usersSubscription.unsubscribe();
+    }
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
